63dfda981ec4d065ddce5f94939751acd9b03bf5
Ramesh Reddy <rareddy@jboss.org>
2020-05-12 17:24:02 -0500

    TEIIDTOOLS-989: regenerated the encypted to see if any improvement

--- .travis/before-deploy.sh
+++ .travis/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 mv ~/.gnupg ~/dot.gnupg  
 
-openssl aes-256-cbc -K $encrypted_24431db6f336_key -iv $encrypted_24431db6f336_iv -in .travis/codesign.gpg.enc -out .travis/codesign.gpg -d
+openssl aes-256-cbc -K $encrypted_1f0e3118fac8_key -iv $encrypted_1f0e3118fac8_iv -in .travis/codesign.gpg.enc -out .travis/codesign.gpg -d
 gpg --import .travis/codesign.gpg
