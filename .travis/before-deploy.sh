fbe158254ca297f8dbe2ad63fac186d2ea6cf3ca
Ramesh Reddy <rareddy@jboss.org>
2020-05-18 11:08:24 -0500

    TEIIDTOOLS-989: regenerated encryption files and keys (#33)

--- .travis/before-deploy.sh
+++ .travis/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 mv ~/.gnupg ~/dot.gnupg  
 
-openssl aes-256-cbc -K $encrypted_334a64c5d9b2_key -iv $encrypted_334a64c5d9b2_iv -in .travis/codesign.asc.enc -out .travis/codesign.asc -d
-gpg --import .travis/codesign.asc
+openssl aes-256-cbc -K $encrypted_334a64c5d9b2_key -iv $encrypted_334a64c5d9b2_iv -in .travis/codesign.gpg.enc -out .travis/codesign.gpg -d
+gpg --import .travis/codesign.gpg
